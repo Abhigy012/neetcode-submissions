3a7f67abf650a19534289242281fb75e99744010
Abhay Goyal <147277054+Abhigy012@users.noreply.github.com>
2026-05-12 11:44:09 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s) != len(t)):
+            return False
+        mpp = dict()
+        for x in s:
+            if x in mpp:
+                mpp[x] += 1
+            else:
+                mpp[x] = 1
+
+        for y in t:
+            if mpp.get(y) == None or mpp[y] == 0:
+                return False
+            else:
+                mpp[y] -= 1
+        return True
+            
